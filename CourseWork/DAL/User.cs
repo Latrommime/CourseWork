089efdaf4992ea19505869593baa12b790b8dacd
latrommime <melniks18@ukr.net>
2020-05-31 19:36:35 +0300

    commit

---
+++ CourseWork/DAL/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.DAL
+{
+    class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+    }
+}
