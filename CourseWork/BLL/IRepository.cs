56c5c26833dcaa05909a1523d476b8da2d6bc5f2
latrommime <melniks18@ukr.net>
2020-06-01 00:03:33 +0300

    Added IRepository

---
+++ CourseWork/BLL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.BLL
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        T Get(string name);
+
+        void Create(T item);
+        void Delete(int id);
+    }
+}
