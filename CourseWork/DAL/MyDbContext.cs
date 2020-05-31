7a0a3583f039d5258f8c89b84c502c888b284d94
latrommime <melniks18@ukr.net>
2020-05-31 22:33:06 +0300

    Added dbContext

---
+++ CourseWork/DAL/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CourseWork.DAL;
+using System.Data.Entity;
+
+namespace CourseWork
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext()
+              : base("name=MyDbContext") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<Auction> Auctions { get; set; }
+
+    }
+}
