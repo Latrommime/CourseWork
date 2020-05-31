005f4bfdd484d443ca3d2d593bff948224082c5e
latrommime <melniks18@ukr.net>
2020-05-31 22:03:48 +0300

    Auction updated

--- CourseWork/Auction.cs
+++ CourseWork/Auction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace CourseWork
 {
     class Auction
     {
+        [Key]
+        public int Id { get; set; }
+        public int LotId { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int MinBid { get; set; }
+        public int CurrentBid { get; set; }
+        public int currentBidUserId { get; set; }
     }
 }
